aea4202bcca92184a22f164a659aa215b268c75e
Jesse Wenger <wengerj@gmail.com>
2021-03-30 21:11:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    if Time.now < Time(12:00:00)
+    if Time.now < Time.parse(12:00:00)
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
